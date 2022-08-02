32ae1458a9a28d91f8f19e1e91cfdb5d83a0ebc9
xuguojing-xgj <2313493661@qq.com>
2022-08-02 10:55:55 +0800

    配置请求拦截 响应拦截 在main.js 导入

--- common/request.js
+++ common/request.js
@@ -1,8 +1,28 @@
+/**
+ * 配置网络请求的方法
+ */
+
 // 按需导入 $http 对象
-import { $http } from '@escook/request-miniprogram'
+import {
+	$http
+} from '@escook/request-miniprogram'
+
+
+
+// 配置请求的根路径
+$http.baseUrl = 'https://api-hmugo-web.itheima.net/api/public/v1'
+
+// 请求拦截 请求开始之前做一些事情
+$http.beforeRequest = function(options) {
+	wx.showLoading({
+		title: '数据加载中...',
+	})
+}
+
+// 响应拦截 请求完成之后做一些事情
+$http.afterRequest = function() {
+	wx.hideLoading()
+}
 
 // 在 uni-app 项目中，可以把 $http 挂载到 uni 顶级对象之上，方便全局调用
 uni.$http = $http
-
-// 配置请求的根路径
-uni.$http.baseUrl = 'https://api-hmugo-web.itheima.net/api/public/v1'
--- main.js
+++ main.js
@@ -1,12 +1,14 @@
-
 import Vue from 'vue'
 import App from './App'
 
 Vue.config.productionTip = false
 
+//  导入请求配置文件
+import '@/common/request.js'
+
 App.mpType = 'app'
 
 const app = new Vue({
-    ...App
+	...App
 })
 app.$mount()
