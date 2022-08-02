242037ea214c0e3075aa683310cf081f538d4a26
xuguojing-xgj <2313493661@qq.com>
2022-08-02 14:06:13 +0800

    request interface swiper data package function

--- pages/Home/Home.vue
+++ pages/Home/Home.vue
@@ -8,8 +8,28 @@
 	export default {
 		data() {
 			return {
-				
+				swiperList: [], // 轮播图
 			};
+		},
+		//  页面加载时 显示的数据
+		onLoad() {
+			this.getSwiperList()
+		},
+		methods: {
+			async getSwiperList() {
+				const {
+					datat: res
+				} = uni.$http.get('/home/swiperdata')
+
+				if (res.meta.status != 200) {
+					return uni.showToast({
+						title: "数据获取失败",
+						icon: "none"
+					})
+				}
+				this.swiperList = res.message
+				console.log(res)
+			}
 		}
 	}
 </script>
