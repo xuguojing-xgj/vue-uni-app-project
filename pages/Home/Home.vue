3d1a4fe6404a716f7ef44dbd1e0c4dad1c25b45f
xuguojing-xgj <2313493661@qq.com>
2022-08-02 17:10:46 +0800

    实现 swiper 效果

--- pages/Home/Home.vue
+++ pages/Home/Home.vue
@@ -1,6 +1,13 @@
 <template>
 	<view>
-		首页
+		<!-- 轮播图 -->
+		<swiper indicator-dots autoplay :interval="3000" :duration="1000">
+			<swiper-item v-for="(item, index) in swiperList" :key="item.goods_id">
+				<view class="swiper-item">
+					<image :src="item.image_src"></image>
+				</view>
+			</swiper-item>
+		</swiper>
 	</view>
 </template>
 
@@ -16,17 +23,16 @@
 			this.getSwiperList()
 		},
 		methods: {
+			// 调用接口 发送网络请求
 			async getSwiperList() {
-				const {
-					datat: res
-				} = uni.$http.get('/home/swiperdata')
-
+				const { data : res } = await uni.$http.get('/home/swiperdata')
 				if (res.meta.status != 200) {
 					return uni.showToast({
 						title: "数据获取失败",
 						icon: "none"
 					})
 				}
+				// 赋值数据
 				this.swiperList = res.message
 				console.log(res)
 			}
@@ -35,5 +41,13 @@
 </script>
 
 <style lang="scss">
+	swiper {
+		height: 350rpx;
 
+		.swiper-item,
+		image {
+			height: 100%;
+			width: 100%;
+		}
+	}
 </style>
