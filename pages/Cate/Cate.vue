e1e463c9a77fb98f7c3f2ccad38dcb02b70a1330
xuguojing-xgj <2313493661@qq.com>
2022-08-05 13:00:40 +0800

    搭建分类模板

--- pages/Cate/Cate.vue
+++ pages/Cate/Cate.vue
@@ -1,6 +1,49 @@
 <template>
 	<view>
-		分类
+		<view class="scroll-view-cantainer">
+			<!-- 左侧滚动视图 -->
+			<scroll-view class="left-scroll-view" scroll-y="true">
+				<view>左侧模板</view>
+				<view>左侧模板</view>
+				<view>左侧模板</view>
+				<view>左侧模板</view>
+				<view>左侧模板</view>
+				<view>左侧模板</view>
+				<view>左侧模板</view>
+				<view>左侧模板</view>
+				<view>左侧模板</view>
+				<view>左侧模板</view>
+				<view>左侧模板</view>
+				<view>左侧模板</view>
+				<view>左侧模板</view>
+				<view>左侧模板</view>
+				<view>左侧模板</view>
+				<view>左侧模板</view>
+				<view>左侧模板</view>
+				<view>左侧模板</view>
+				<view>左侧模板</view>
+				<view>左侧模板</view>
+				<view>左侧模板</view>
+			</scroll-view>
+			<!-- 右侧滚动视图 -->
+			<scroll-view class="right-scroll-view" scroll-y="true">
+				<view>右侧模板</view>
+				<view>右侧模板</view>
+				<view>右侧模板</view>
+				<view>右侧模板</view>
+				<view>右侧模板</view>
+				<view>右侧模板</view>
+				<view>右侧模板</view>
+				<view>右侧模板</view>
+				<view>右侧模板</view>
+				<view>右侧模板</view>
+				<view>右侧模板</view>
+				<view>右侧模板</view>
+				<view>右侧模板</view>
+				<view>右侧模板</view>
+				<view>右侧模板</view>
+			</scroll-view>
+		</view>
 	</view>
 </template>
 
@@ -8,12 +51,18 @@
 	export default {
 		data() {
 			return {
-				
+
 			};
 		}
 	}
 </script>
 
 <style lang="scss">
+	.scroll-view-cantainer {
+		display: flex;
 
+		.left-scroll-view {
+			width: 120px;
+		}
+	}
 </style>
