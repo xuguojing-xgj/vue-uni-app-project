6b69de367f0f724dc675ff17894108154af4160c
xuguojing-xgj <2313493661@qq.com>
2022-08-05 13:15:55 +0800

    获取当前系统可用高度

--- pages/Cate/Cate.vue
+++ pages/Cate/Cate.vue
@@ -2,7 +2,25 @@
 	<view>
 		<view class="scroll-view-cantainer">
 			<!-- 左侧滚动视图 -->
-			<scroll-view class="left-scroll-view" scroll-y="true">
+			<scroll-view class="left-scroll-view" scroll-y="true" :style="{height: wh + 'px'}">
+				<view>左侧模板</view>
+				<view>左侧模板</view>
+				<view>左侧模板</view>
+				<view>左侧模板</view>
+				<view>左侧模板</view>
+				<view>左侧模板</view>
+				<view>左侧模板</view>
+				<view>左侧模板</view>
+				<view>左侧模板</view>
+				<view>左侧模板</view>
+				<view>左侧模板</view>
+				<view>左侧模板</view>
+				<view>左侧模板</view>
+				<view>左侧模板</view>
+				<view>左侧模板</view>
+				<view>左侧模板</view>
+				<view>左侧模板</view>
+				<view>左侧模板</view>
 				<view>左侧模板</view>
 				<view>左侧模板</view>
 				<view>左侧模板</view>
@@ -26,7 +44,31 @@
 				<view>左侧模板</view>
 			</scroll-view>
 			<!-- 右侧滚动视图 -->
-			<scroll-view class="right-scroll-view" scroll-y="true">
+			<scroll-view class="right-scroll-view" scroll-y="true" :style="{height: wh + 'px'}">
+				<view>右侧模板</view>
+				<view>右侧模板</view>
+				<view>右侧模板</view>
+				<view>右侧模板</view>
+				<view>右侧模板</view>
+				<view>右侧模板</view>
+				<view>右侧模板</view>
+				<view>右侧模板</view>
+				<view>右侧模板</view>
+				<view>右侧模板</view>
+				<view>右侧模板</view>
+				<view>右侧模板</view>
+				<view>右侧模板</view>
+				<view>右侧模板</view>
+				<view>右侧模板</view>
+				<view>右侧模板</view>
+				<view>右侧模板</view>
+				<view>右侧模板</view>
+				<view>右侧模板</view>
+				<view>右侧模板</view>
+				<view>右侧模板</view>
+				<view>右侧模板</view>
+				<view>右侧模板</view>
+				<view>右侧模板</view>
 				<view>右侧模板</view>
 				<view>右侧模板</view>
 				<view>右侧模板</view>
@@ -51,8 +93,13 @@
 	export default {
 		data() {
 			return {
-
+				wh: 0, // 当前系统窗口可用高度
 			};
+		},
+		onLoad() {
+			// 获取当前系统可用高度
+			const systemInfo = uni.getSystemInfoSync()
+			this.wh = systemInfo.windowHeight
 		}
 	}
 </script>
