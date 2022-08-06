153d7cd650084dfbf902d82f17424f4aced9c6e5
xuguojing-xgj <2313493661@qq.com>
2022-08-06 15:13:07 +0800

    根据关键字查询搜索列表建议

--- subpkg/search/search.vue
+++ subpkg/search/search.vue
@@ -11,6 +11,7 @@
 			return {
 				timer: null, // 定时器
 				keyword: '', // 用户输入的关键字
+				searchResults: [], // 用户搜索建议列表数据
 			};
 		},
 		methods: {
@@ -21,8 +22,28 @@
 				// 重新启动一个延迟器, 并把  timeId 赋值给 this.timer
 				this.timer = setTimeout(() => {
 					this.keyword = e
-					console.log(this.keyword)
+					// console.log(this.keyword)
+					// 调用获取用户搜索列表建议数据方法
+					this.getSearchList()
 				}, 800)
+			},
+			// 获取用户搜索列表建议数据
+			async getSearchList() {
+				// 判断输入框长度是否为 0
+				if (this.keyword.trim().length === 0) {
+					this.searchResults = []
+					return
+				}
+				// 请求数据
+				const {
+					data: res
+				} = await uni.$http.get('/goods/qsearch', {
+					query: this.keyword
+				})
+				if (res.meta.status != 200) return uni.$showMsg()
+				this.searchResults = res.message
+				console.log(res)
+				console.log(this.keyword.trim().length)
 			},
 			// 当点击搜索框时,  用户按下回车触发事件
 			confirm(e) {
