c1f52fb03e7cd1b44b1ee8f014e5a5c5b10cd66b
xuguojing-xgj <2313493661@qq.com>
2022-08-06 14:01:16 +0800

    给搜索框添加防抖函数

--- subpkg/search/search.vue
+++ subpkg/search/search.vue
@@ -9,13 +9,20 @@
 	export default {
 		data() {
 			return {
-
+				timer: null, // 定时器
+				keyword: '', // 用户输入的关键字
 			};
 		},
 		methods: {
 			// 当输入框发生改变时 触发事件
 			input(e) {
-				console.log(e)
+				// 清除 timeId 对应的延迟器
+				clearTimeout(this.timer)
+				// 重新启动一个延迟器, 并把  timeId 赋值给 this.timer
+				this.timer = setTimeout(() => {
+					this.keyword = e
+					console.log(this.keyword)
+				}, 800)
 			},
 			// 当点击搜索框时,  用户按下回车触发事件
 			confirm(e) {
