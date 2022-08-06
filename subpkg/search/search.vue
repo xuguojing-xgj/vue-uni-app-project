1efeae83bb9026cdb55eb5c0d6953d85bc8f8a86
xuguojing-xgj <2313493661@qq.com>
2022-08-06 13:50:16 +0800

    自动获取焦点

--- subpkg/search/search.vue
+++ subpkg/search/search.vue
@@ -1,7 +1,7 @@
 <template>
 	<view>
 		<!-- bgColor="#c00000" 直接使用组件内置的属性修改 -->
-		<uni-search-bar :radius="20" cancelButton="none"></uni-search-bar>
+		<uni-search-bar :radius="20" cancelButton="none" @input="input" @confirm="confirm"></uni-search-bar>
 	</view>
 </template>
 
@@ -11,6 +11,16 @@
 			return {
 
 			};
+		},
+		methods: {
+			// 当输入框发生改变时 触发事件
+			input(e) {
+				console.log(e)
+			},
+			// 当点击搜索框时,  用户按下回车触发事件
+			confirm(e) {
+				console.log(e)
+			}
 		}
 	}
 </script>
