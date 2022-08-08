d39415fde848ca07ea46e971633d5dffcc192216
xuguojing-xgj <2313493661@qq.com>
2022-08-08 09:43:03 +0800

    渲染商品列表 ui 完成

--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,6 +1,31 @@
 <template>
 	<view>
-		商品列表
+		<view class="goods-list">
+			<!--  block 不是一个组件，就是一个标签，这个标签一般就是配置 渲染和跳转判断来使用的 -->
+			<block v-for="(goods, i) in goodsList" :key="i">
+				<view class="goods-item">
+
+					<view class="goods-item-left">
+						<image :src="goods.goods_small_logo || defaultPic" class="goods-pic"></image>
+					</view>
+
+					<!-- 商品右侧信息区域 -->
+					<view class="goods-item-right">
+						<!-- 商品标题 -->
+						<view class="goods-name">
+							{{goods.goods_name}}
+						</view>
+						<view class="goods-info-box">
+							<!-- 商品价格 -->
+							<view class="goods-price">
+								{{goods.goods_price}}
+							</view>
+
+						</view>
+					</view>
+				</view>
+			</block>
+		</view>
 	</view>
 </template>
 
@@ -45,5 +70,44 @@
 </script>
 
 <style lang="scss">
+	.goods-item {
+		display: flex;
+		padding: 10px 5px;
+		border-bottom: 1px solid #f0f0f0;
 
+		.goods-item-left {
+			margin-right: 5px;
+			// <!-- 添加 radio 2： 设置样式 -->
+			display: flex;
+			justify-content: space-between;
+			align-items: center;
+
+			.goods-pic {
+				width: 100px;
+				height: 100px;
+				display: block;
+			}
+		}
+
+		.goods-item-right {
+			display: flex;
+			flex-direction: column;
+			justify-content: space-between;
+
+			.goods-name {
+				font-size: 13px;
+			}
+
+			.goods-info-box {
+				display: flex;
+				justify-content: space-between;
+				align-items: center;
+			}
+
+			.goods-price {
+				font-size: 16px;
+				color: #c00000;
+			}
+		}
+	}
 </style>
