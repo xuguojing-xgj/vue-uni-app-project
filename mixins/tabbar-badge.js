b38dfccb47c23e4df1433db33a2ca009e3845e92
xuguojing-xgj <2313493661@qq.com>
2022-08-11 09:47:59 +0800

    解决购买改变徽章不改变的 bug

--- mixins/tabbar-badge.js
+++ mixins/tabbar-badge.js
@@ -5,24 +5,29 @@
 
 
 import {
-		mapGetters
-	} from 'vuex'
-	export default {
-		onShow() {
-			// 调用设置 tabBar 徽章的方法
-			this.setBadge()
-		},
-		methods: {
-			// 设置 tabBar 徽章的方法
-			setBadge() {
-				// 调用 api 设置徽章
-				uni.setTabBarBadge({
-					index: 2,
-					text: this.total + '' // 参数必须是 string 类型
-				})
-			}
-		},
-		computed: {
-			...mapGetters('m_cart', ['total'])
+	mapGetters
+} from 'vuex'
+export default {
+	onShow() {
+		// 调用设置 tabBar 徽章的方法
+		this.setBadge()
+	},
+	methods: {
+		// 设置 tabBar 徽章的方法
+		setBadge() {
+			// 调用 api 设置徽章
+			uni.setTabBarBadge({
+				index: 2,
+				text: this.total + '' // 参数必须是 string 类型
+			})
 		}
+	},
+	watch: {
+		total() {
+			this.setBadge()
+		}
+	},
+	computed: {
+		...mapGetters('m_cart', ['total'])
 	}
+}
